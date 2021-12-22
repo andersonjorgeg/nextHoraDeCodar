87dd19046db3f972ffd7023a122ae2819537ab04
Anderson Jorge <anderson.jorge33@gmail.com>
2021-12-22 18:18:24 -0300

    assets

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -11,6 +12,12 @@ export default function Home() {
       </Head>
       <div>
       <h1 className={styles.title}>Hello World</h1>
+      <Image 
+        src="/images/city.jpg" 
+        width="350" 
+        height="450" 
+        alt="cidade a noite" 
+      />
     </div>
     </>
   )
